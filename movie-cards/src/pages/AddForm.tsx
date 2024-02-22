bc8cd0dc318274511229cf3ce22b6a5ac32cbb40
Sarbast Hesso <sarbast.hesso@gmail.com>
2024-02-22 11:06:58 +0100

    form

--- movie-cards/src/pages/AddForm.tsx
+++ movie-cards/src/pages/AddForm.tsx
@@ -1,33 +1,67 @@
 
+interface IMovieGenre {
+    id: number;
+    name: string;
+}
+
 const AddForm = () => {
+
+    const genreCategories:IMovieGenre [] = [
+        {id: 1, name: 'drama'},
+        {id: 2, name: 'comedy'},
+        {id: 3, name: 'fantasy'},
+        {id: 4, name: 'horror'},
+        {id: 5, name: 'romance'},
+        {id: 6, name: 'action'},
+    ]
+
   return (
-    <form className='add-form'>
-        <div className="form-group">
-            <label htmlFor="title">Title</label>
-            <input type="text" name="title" id="title" />
+    <form className="add-form">
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" id="title" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="rating">Rating</label>
+        <div className="rating-wrapper">
+          <span>1</span>
+          <input
+            type="range"
+            name="rating"
+            id="rating"
+            min="1"
+            max="5"
+            value="2"
+          />
+          <span>5</span>
         </div>
-        <div className="form-group">
-            <label htmlFor="rating">Rating</label>
-            <input type="range" name="rating" id="rating" min="1" max="5" value="2"/>
-        </div>
-        <div className="form-group">
-            <label htmlFor="genre">Genre</label>
-            <select name="genre" id="genre">
-                <option value="drama">Drama</option>
-                <option value="horror">Horror</option>
-                <option value="comedy">Comedy</option>
-            </select>
-        </div>
-        <div className="form-group">
-            <label htmlFor="description">Description</label>
-            <textarea name="description" id="description" />
-        </div>
-        <div className="btns">
-            <button>Clear</button>
-            <button>Add</button>
-        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="genre">Genre</label>
+        <select name="genre" id="genre">
+          {
+            genreCategories.map((category) => {
+                <option defaultValue={category.name} key={category.id}>{category.name}</option>;
+            })
+          }
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="description">Description</label>
+        <textarea name="description" id="description" />
+      </div>
+      <div className="btns">
+        <button className="add-btn">ADD</button>
+        <button className="clear-btn">CLEAR</button>
+      </div>
     </form>
-  )
+  );
 }
 
 export default AddForm
+
+
+
+
+
+
